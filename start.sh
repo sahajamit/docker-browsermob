991e179905acc71e8a5bae30be1f1fd1acd71f7f
Amit Rawat <amit.rawat@equinox.com>
2015-11-11 17:12:07 +0530

    some more changes

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 
 # Give server time to boot, then setup proxy on port 8019
 (sleep 5 && curl -sSX POST -d 'port=8019' http://0.0.0.0:8080/proxy) && sleep 2 && curl -H "Content-Type: application/json" -X POST -d '{"username":"admin","password":"admin"}' http://0.0.0.0:8080/proxy/8019/auth/basic/the-internet.herokuapp.com/ &
-./browsermob-proxy -port 8080 --use-littleproxy false
+exec /browsermob-proxy-2.1.0-beta-3/bin/browsermob-proxy -port 8080 --use-littleproxy false

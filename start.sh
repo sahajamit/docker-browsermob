4ba94b181a61917531af4a99a6cdcdec151c8aae
Amit Rawat <amit.rawat@equinox.com>
2015-11-11 17:26:19 +0530

    added more hosts fro http basic auth

--- start.sh
+++ start.sh
@@ -3,5 +3,8 @@
 #!/bin/bash
 
 # Give server time to boot, then setup proxy on port 8019
-(sleep 10 && curl -sSX POST -d 'port=8019' http://0.0.0.0:8080/proxy) && sleep 2 && curl -H "Content-Type: application/json" -X POST -d '{"username":"admin","password":"admin"}' http://0.0.0.0:8080/proxy/8019/auth/basic/the-internet.herokuapp.com/ &
+(sleep 10 && curl -sSX POST -d 'port=8019' http://0.0.0.0:8080/proxy) &&
+sleep 2 &&
+curl -H "Content-Type: application/json" -X POST -d '{"username":"admin","password":"admin"}' http://0.0.0.0:8080/proxy/8019/auth/basic/the-internet.herokuapp.com/ &&
+curl -H "Content-Type: application/json" -X POST -d '{"username":"WebPreview","password":"Equinox1!"}' http://0.0.0.0:8080/proxy/8019/auth/basic/stag.equinox.com/ &
 exec /browsermob-proxy-2.1.0-beta-3/bin/browsermob-proxy -port 8080 --use-littleproxy false

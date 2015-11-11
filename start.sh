ada2097f47b3c00f47ef20f246a42eb047356c62
Amit Rawat <amit.rawat@equinox.com>
2015-11-11 17:38:30 +0530

    some changes in password

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@
 (sleep 10 && curl -sSX POST -d 'port=8019' http://0.0.0.0:8080/proxy) &&
 sleep 2 &&
 curl -H "Content-Type: application/json" -X POST -d '{"username":"admin","password":"admin"}' http://0.0.0.0:8080/proxy/8019/auth/basic/the-internet.herokuapp.com/ &&
-curl -H "Content-Type: application/json" -X POST -d '{"username":"WebPreview","password":"Equinox1!"}' http://0.0.0.0:8080/proxy/8019/auth/basic/stag.equinox.com/ &
+curl -H "Content-Type: application/json" -X POST -d '{"username":"DevPreview","password":"Equinox1!"}' http://0.0.0.0:8080/proxy/8019/auth/basic/stag.equinox.com/ &
 exec /browsermob-proxy-2.1.0-beta-3/bin/browsermob-proxy -port 8080 --use-littleproxy false

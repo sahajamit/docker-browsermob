9205a258fde30e92fd7eb27245bac3af67cc0bb8
Amit Rawat <amit.rawat@equinox.com>
2015-11-11 17:14:42 +0530

    increased sleep from 5 to 10 secs

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 #!/bin/bash
 
 # Give server time to boot, then setup proxy on port 8019
-(sleep 5 && curl -sSX POST -d 'port=8019' http://0.0.0.0:8080/proxy) && sleep 2 && curl -H "Content-Type: application/json" -X POST -d '{"username":"admin","password":"admin"}' http://0.0.0.0:8080/proxy/8019/auth/basic/the-internet.herokuapp.com/ &
+(sleep 10 && curl -sSX POST -d 'port=8019' http://0.0.0.0:8080/proxy) && sleep 2 && curl -H "Content-Type: application/json" -X POST -d '{"username":"admin","password":"admin"}' http://0.0.0.0:8080/proxy/8019/auth/basic/the-internet.herokuapp.com/ &
 exec /browsermob-proxy-2.1.0-beta-3/bin/browsermob-proxy -port 8080 --use-littleproxy false

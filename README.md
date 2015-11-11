06bf7dcfc1af5d2bdc49edbe2e196bf445b9b741
Amit Rawat <amit.rawat@equinox.com>
2015-11-11 16:05:12 +0530

    updated the readme file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # docker-browsermob
+BrowserMob Proxy 2.1 in Docker
+==========================
+
+This is a simple docker image with browsermob-proxy-2.1 version.
+
+By default, it will load BrowserMob-Proxy on port 8080, and start a proxy listener on port 8019.
+
+Usage
+-----
+
+    $ docker run -p 8080:8080 -p 8019:8019 sahajamit/docker-browsermob
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Give server time to boot, then setup proxy on port 8019
-(sleep 3 && curl -sSX POST -d "port=8019&bindAddress=$(cat /etc/hosts | grep $(hostname) | cut -f1)" http://0.0.0.0:8080/proxy) &
+#(sleep 10 && curl -sSX POST -d "port=8019&bindAddress=$(cat /etc/hosts | grep $(hostname) | cut -f1)" http://0.0.0.0:8080/proxy) &
 
 exec /browsermob-proxy-2.1.0-beta-3/bin/browsermob-proxy -port 8080 --use-littleproxy false
